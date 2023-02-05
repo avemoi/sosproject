ca459950d900db26fa4e50bef29973bd4f8bc008
avemoi <cmageiridis@protonmail.com>
2023-02-05 14:27:20 +0200

    set new token

--- cmd/api/middleware.go
+++ cmd/api/middleware.go
@@ -6,9 +6,10 @@ import (
 )
 
 func TokenAuthentication() gin.HandlerFunc {
+	authToken := "=5RZsn+x*B/(-UA;6W"
 	return func(c *gin.Context) {
 		authHeader := c.Request.Header.Get("Authorization")
-		if authHeader != "mytoken" {
+		if authHeader != authToken {
 			c.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{
 				"error": "Unauthorized",
 			})
